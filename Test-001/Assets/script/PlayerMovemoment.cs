8d46e975a8b37b35725d65128fdf61e29f598bf0
Wizz <108207747+WizzZn@users.noreply.github.com>
2024-05-15 13:08:31 +0530

    Movement & Jump Update002
    *Jump update
    *Movement Update
    *Sprite Update

--- Test-001/Assets/script/PlayerMovemoment.cs
+++ Test-001/Assets/script/PlayerMovemoment.cs
@@ -5,31 +5,47 @@ using UnityEngine;
 
 public class PlayerMovemoment : MonoBehaviour
 {
-    [SerializeField] int speed;
-    [SerializeField] bool groundi= false;
-    [SerializeField] Rigidbody2D rb;
-
-     Vector2 pos;
+    //[SerializeField] int speed;
+    [SerializeField] bool isgrounded;
+    [SerializeField] int jumpPower;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] int horizonSpeed;
+   
+    
+    Rigidbody2D rb;
+    Vector2 pos;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        pos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
         Movement();
+      /*  float Horizon = Input.GetAxis("Horizontal") * horizonSpeed * Time.deltaTime;
+        pos = new Vector2(Horizon, pos.y);
+        transform.position = pos;*/
 
     }
     void Movement()
     {
-        pos = transform.position;
-        pos.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        pos.y = Mathf.Clamp(pos.y, -5f, 2.5f);
-        transform.position = pos;
-        //float Horizone = Input.GetAxis("Horizontal")* 
+        isgrounded = Physics2D.OverlapCapsule(groundCheck.position, new Vector2(0.7f, 0.3f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+        if (Input.GetButtonDown("Jump") && isgrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            rb.velocity = new Vector2(horizonSpeed, rb.velocity.y);
+        }
+
+       
+
     }
 
 }
